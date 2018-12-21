e2460ae20ae479015b30ad59df3ba474d0a574bb
Uki7991 <tilek.kubanov@gmail.com>
2018-12-21 18:09:25 +0600

    vuex learning

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ count }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
   </div>
 </template>
+
+<script>
+  import store from '../Vuex'
+
+  export default {
+    name: 'About',
+    computed: {
+      count() {
+        return store.state.count;
+      }
+    },
+    methods: {
+      increment() {
+        store.commit('increment');
+      },
+      decrement() {
+        store.commit('decrement');
+      }
+    }
+  }
+</script>
